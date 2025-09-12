c9cc933181f02a2811c0e09d9d19db010bb50f51
Thanh279 <drawin278@gmail.com>
2025-09-12 19:47:01 +0700

    feat(layouts): thêm footer và service-list layout
    - Tạo mới file `footer.blade.php` để quản lý phần footer
    - Tạo mới file `service-list.blade.php` để hiển thị danh sách dịch vụ
    - Cập nhật giao diện trong các trang admin:
      * contacts/index.blade.php
      * contacts/show.blade.php
      * media/index.blade.php
      * news/index.blade.php
      * services/index.blade.php
      * settings/index.blade.php

---
+++ resources/views/layouts/service-list.blade.php
@@ -0,0 +1,18 @@
+<div class="cl-sec01-cate" data-aos="zoom-in" data-aos-duration="3000">
+    <div class="row">
+        @foreach($services as $service)
+            <div class="col-12 col-sm-3">
+                <div class="col-item">
+                    <a href="{{ route('services.detail', $service->slug) }}" class="a-cate-item">
+                        @if($service->icon)
+                            <img src="{{ asset('storage/' . $service->icon) }}" alt="{{ $service->name }}">
+                        @else
+                            <img src="{{ asset('images/home/default_icon.png') }}" alt="{{ $service->name }}">
+                        @endif
+                        <label>{{ $service->name }}</label>
+                    </a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
