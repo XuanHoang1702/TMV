5d4f0f5d5c09ac2c5f66a5d8748c4ba41110f2eb
XuanHoang1702 <tranxuanhoang17022004@gmail.com>
2025-09-13 19:30:48 +0700

    Create table Site_info

---
+++ app/Models/SiteInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SiteInfo extends Model
+{
+    use HasFactory;
+    protected $table = 'site_info';
+    protected $fillable = [
+        'id',
+        'logo',
+        'slogan'
+    ];
+}
